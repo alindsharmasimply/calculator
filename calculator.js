0494cc24a1384b210cf71d1f5c808d1aa85214ee
alindsharmasimply <alindsharmasimply@gmail.com>
2020-01-22 21:17:41 +0530

    BMI Calculator Only

---
+++ calculator.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(request, response){
+	response.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post('/bmicalculator', function(request, response){
+	var num1 = parseFloat(request.body.weight);
+	var num2 = parseFloat(request.body.height);
+	var result = num1 / (num2 * num2);
+	response.send("Your BMI is " + result);
+});
+
+app.listen(3000, function(){
+	console.log("Listening to port 3000");
+})
